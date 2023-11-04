cf4a29d0c8f3b734ef22fd799d2e6c1ddef7b244
vijay <vijay2340025@gmail.com>
2023-11-04 08:09:16 +0530

    feat: add cloud-config server

--- config-server/src/main/java/com/example/ConfigServer.java
+++ config-server/src/main/java/com/example/ConfigServer.java
@@ -3,13 +3,13 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RefreshScope
-public class ApiGateway {
+@EnableConfigServer
+public class ConfigServer {
     public static void main(String[] args) {
-        SpringApplication.run(ApiGateway.class, args);
+        SpringApplication.run(ConfigServer.class, args);
     }
 }
